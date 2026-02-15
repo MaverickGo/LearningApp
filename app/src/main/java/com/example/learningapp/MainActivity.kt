5b37adf77e15b4fa2b4b9ea4b28d5f0d63fbb418
MaverickGo <amir.orlovskikh@gmail.com>
2026-02-15 13:01:12 +0200

    Added one block more

--- app/src/main/java/com/example/learningapp/MainActivity.kt
+++ app/src/main/java/com/example/learningapp/MainActivity.kt
@@ -4,18 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -27,17 +31,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LearningAppTheme {
-                Text(
-                    text = "Hello World!"
-                )
+                Content("Mirasvet")
             }
         }
     }
 }
 
-@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun Content(name: String = "Default") {
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -45,6 +46,36 @@ fun GreetingPreview() {
             .padding(top = 50.dp),
         contentAlignment = Alignment.TopCenter
     ) {
-        Text(text = "Hello World!", fontSize = 20.sp, color = Color(red = 255, green = 0, blue = 0))
+        Text(
+            text = "Hello World!",
+            modifier = Modifier
+                .background(Color.Yellow)
+                .padding(all = 20.dp),
+            fontSize = 20.sp,
+            color = Color(red = 255, green = 0, blue = 0)
+        )
+        Text(
+            text = "Your name is $name",
+            modifier = Modifier
+                .padding(top = 100.dp)
+                .background(
+                    shape = CircleShape,
+                    color = Color.Green
+                )
+                .padding(top = 5.dp)
+                .width(300.dp)
+                .height(50.dp),
+            fontSize = 20.sp,
+            color = Color.Blue,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    LearningAppTheme {
+        Content("Mirasvet")
     }
 }
