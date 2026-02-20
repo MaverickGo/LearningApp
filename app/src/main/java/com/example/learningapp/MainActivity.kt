f7e4edb593358a3ece00f3b1c0f93edf0bbff019
MaverickGo <amir.orlovskikh@gmail.com>
2026-02-20 14:57:26 +0200

    Created background and header, added custom fontFamily

--- app/src/main/java/com/example/learningapp/MainActivity.kt
+++ app/src/main/java/com/example/learningapp/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.learningapp.ui.theme.LearningAppTheme
+import com.example.learningapp.ui.theme.PlayfairDisplay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,9 +30,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LearningAppTheme {
-                Scaffold(modifier = Modifier.padding()) {innerPadding ->
+                //Scaffold(modifier = Modifier.padding()) { innerPadding ->
 
-                }
+                //}
             }
         }
     }
@@ -30,13 +42,49 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Preview() {
     LearningAppTheme {
-        Scaffold(modifier = Modifier.padding()) {innerPadding ->
-            Screen()
+        Scaffold(modifier = Modifier.padding()) { innerPadding ->
+            BackgroundColor(0xFFF0FFFF)
+            Surface(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize(),
+                color = (Color(0x00FF00FF))
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(innerPadding)
+                        .fillMaxSize()
+                ) {
+                    Greeting()
+                }
+            }
         }
     }
 }
 
 @Composable
-fun Screen() {
+fun BackgroundColor(colorHex: Long) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(colorHex))
+    )
+}
 
+@Composable
+fun Greeting() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 10.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Welcome to my ToDoList!",
+            fontSize = 26.sp,
+            color = Color(0xFF4169E1),
+            fontFamily = PlayfairDisplay,
+            fontWeight = FontWeight.Bold
+        )
+    }
 }
